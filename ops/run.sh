5f5312d6e573f5d778b6b4457a56036271301afe
Ryan Cargan <ryancargan@gmail.com>
2022-10-11 09:44:05 +0530

    Add shell script for running both apps
    Add shell signal trap to simultaneously run both the frontend
    and backend.
    
    The frontend gets backgrounded while only the backend prints to stdout.
    
    The trap allows a Ctrl+C interrupt to stop both without having to
    use a task manager to kill the backgrounded process.

--- ops/run.sh
+++ ops/run.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 (trap 'kill 0' SIGINT;
+	cd ../frontend/client/ &&
+	pnpm dev &
 	cd ../backend/api/ &&
-	./mvnw spring-boot:run &
-	cd ../../frontend/client/ &&
-	pnpm build && pnpm preview
+	./mvnw spring-boot:run
 )
