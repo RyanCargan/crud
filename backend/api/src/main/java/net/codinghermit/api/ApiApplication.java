b6774d72e1b42b3f962a6ad1d257ceb5e5c7d97e
Ryan Cargan <ryancargan@gmail.com>
2022-10-13 21:29:27 +0530

    Add functional basic REST API

--- backend/api/src/main/java/net/codinghermit/api/ApiApplication.java
+++ backend/api/src/main/java/net/codinghermit/api/ApiApplication.java
@@ -1,14 +1,13 @@
 package net.codinghermit.api;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("net.codinghermit.api.repo")
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.
+         run(ApiApplication.class, args);
+   }
 }
