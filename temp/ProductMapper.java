c656a9ab13b0a98654dc63b5bea0bd886f673620
Ryan Cargan <ryancargan@gmail.com>
2022-10-15 12:49:45 +0530

    Add YAML config & clean up experiments

---
+++ temp/ProductMapper.java
@@ -0,0 +1,14 @@
+package net.codinghermit.api.model;
+
+import net.codinghermit.api.model.Product;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface ProductMapper {
+    Product select(
+            @Param("id")
+                    long id);
+
+    void update(Product product);
+}
