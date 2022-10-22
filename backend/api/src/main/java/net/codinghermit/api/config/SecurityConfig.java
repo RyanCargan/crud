947d61dfc63c0018232100b8ddea2040dcf2a186
Ryan Cargan <ryancargan@gmail.com>
2022-10-22 21:03:15 +0530

    Add basic auth starter boilerplate

--- backend/api/src/main/java/net/codinghermit/api/config/SecurityConfig.java
+++ backend/api/src/main/java/net/codinghermit/api/config/SecurityConfig.java
@@ -1,23 +1,31 @@
 package net.codinghermit.api.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-
-		http.authorizeRequests().antMatchers("/**").permitAll();
-		http.csrf().disable();
-
-		return http.build();
-    }
 }
+
+// package net.codinghermit.api.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+// 		http.authorizeRequests().antMatchers("/**").permitAll();
+// 		http.csrf().disable();
+
+// 		return http.build();
+//     }
+// }
 
 // package net.codinghermit.api;
 
