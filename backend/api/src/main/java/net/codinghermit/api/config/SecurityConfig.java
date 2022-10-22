d7dedd6e8c1c6c5ac8bf0260b5997e9a195378f5
Ryan Cargan <ryancargan@gmail.com>
2022-10-22 23:28:21 +0530

    Update WebSecurityConfigurerAdapter to SecurityFilterChain

--- backend/api/src/main/java/net/codinghermit/api/config/SecurityConfig.java
+++ backend/api/src/main/java/net/codinghermit/api/config/SecurityConfig.java
@@ -3,15 +3,17 @@ package net.codinghermit.api.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     private DataSource dataSource;
@@ -21,18 +23,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
             .dataSource(dataSource)
             .usersByUsernameQuery("select username, \"password\", enabled from users where username=?")
-            .authoritiesByUsernameQuery("select username, \"role\" from users where username=?")
-        ;
+            .authoritiesByUsernameQuery("select username, \"role\" from users where username=?");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .anyRequest().authenticated()
             .and()
             .formLogin().permitAll()
             .and()
             .logout().permitAll();
+
+						return http.build();
     }
 }
 
